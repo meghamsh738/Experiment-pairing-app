33c2ab6b4a74cad6286c1cb455d49af6985e279f
Meghamsh <megha@bio172037.college.tcd.ie>
2025-11-30 10:34:14 +0000

    Use relative WSL path in launcher

--- Run-App.bat
+++ Run-App.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
-set "WSL_PATH='<PROJECTS_DIR>/Experiment-pairing-app/modern-app'"
-start "Experiment pairing servers" wsl -e bash -lc "cd %WSL_PATH% && npm run dev:full"
+for /f "usebackq tokens=*" %%i in (`wsl wslpath "%~dp0"`) do set WSL_DIR=%%i
+start "Experiment pairing servers" wsl -e bash -lc "cd \"%WSL_DIR%modern-app\" && npm run dev:full"
 timeout /t 4 >nul
 start "" http://localhost:5173
 endlocal
